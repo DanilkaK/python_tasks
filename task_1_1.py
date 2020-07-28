7e9a7050f11596671c839fd2a675cd59d9e7aa31
DanilkaK <danik.krasko@mail.ru>
2020-07-28 22:51:31 +0300

    init test

---
+++ task_1_1.py
@@ -0,0 +1,16 @@
+a = float(input('Введети первое число: '))
+b = float(input('Введети второе число: '))
+c = input('Что делаем: + ,-, * : ')
+if c == '+':
+    print('Ответ: ' + str(a + b))
+elif c =='-':
+    print('Ответ: ' + str( a - b))
+else:
+    print('Ответ: ' + str(a * b))
+
+
+
+
+
+
+
---
+++ task_2_4.py
@@ -0,0 +1,4 @@
+s6 = input('Введите что-то: ')
+q = int(len(s6))
+y = q - 2
+print(s6[0:y])
